ddce96419b935fb8dd54cd61f1862bf400648fa2
Vincent Moitry <vincent@et9.fr>
2014-03-11 16:29:01 +0100

    Ajout structure utilisateur
    -> Ajout des structures de connexion, déconnexion et d'inscription ...

--- B-ClientWeb/Client-Web/view/chatView.php
+++ B-ClientWeb/Client-Web/view/chatView.php
@@ -1,20 +1,60 @@
 <body>
 	<div id="chatBox">
-		<div id="chatInfo">
-			Chat Auguste v0.5
-		</div>
+<?php 
+		// Si déconnecté
+		if(false)
+		{
+?>
+			<div>Nom d'utilisateur : ?</div>
+			<button onclick="alert('Envoi déconnexion');">Déconnexion</button>
+			
+			<!-- Div d'affichage des messages -->
+			<div id="chatMessages">
+				Bonjour et bienvenue sur Auguste ...
+			</div>
+
+			<br/>
+			<!-- Div d'envoi d'un message-->
+			<div id="chatSend">
+				<form onSubmit="addMessage(); return false;" autocomplete="off" >
+					<input type="text" id="message" name="message" />
+					<input type="submit" id="valid" value="Envoyer">
+				</form>
+			</div>
+<?php
+		}
 		
-		<div id="chatMessages">
-			Bonjour et bienvenue sur Auguste ...
-		</div>
+		// Si connecté
+		else
+		{
+?>
+			<!-- Connexion -->
+			<fieldset style="display:inline-block;">
+				<legend>Connexion</legend>
+				<form onSubmit="alert('Envoi Connexion'); return false;" autocomplete="off" >
+					<label for="user">Pseudo : </label>
+					<input type="text" name="user" id="user" placeholder="Pseudo">
+					<label for="password">Mot de passe : </label>
+					<input type="password" name="pass" id="password" placeholder="Mot de passe">
+					<button type="submit">Me connecter</button>
+				</form>
+			</fieldset>
 
-		<br/>
-
-		<div id="chatSend">
-			<form onSubmit="addMessage(); return false;" autocomplete="off" >
-				<input type="text" id="message" name="message" />
-				<input type="submit" id="valid" value="Envoyer">
-			</form>
-		</div>
+			<!-- Inscription -->
+			<fieldset style="display:inline-block;">
+				<legend>Inscription</legend>
+				<form onSubmit="alert('Envoi Inscription'); return false;" autocomplete="off" >
+					<label for="user">Pseudo : </label>
+					<input type="text" name="user" id="user" placeholder="Pseudo">
+					<label for="password">Mot de passe : </label>
+					<input type="password" name="pass" id="password" placeholder="Mot de passe">
+					<label for="configrm">Confirmation : </label>
+					<input type="password" name="conf" id="confirm" placeholder="Confirmer">
+					<button type="submit">M'inscrire</button>
+				</form>
+			</fieldset>
+<?php
+		}
+?>
 	</div>
 </body>
