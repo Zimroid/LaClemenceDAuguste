fe0eb22eb1799edc7fc08a0b23fba7d1ed12e693
Vincent Moitry <vincent@moitry.net>
2014-03-30 11:26:55 +0200

    Let's draw some haxagons ...

--- D-ClientGDX/AugusteGDX/src/auguste/client/graphical/GameScreen.java
+++ D-ClientGDX/AugusteGDX/src/auguste/client/graphical/GameScreen.java
@@ -5,9 +5,11 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.PolygonRegion;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 
@@ -18,6 +20,7 @@ public class GameScreen implements Screen {
     Game g;
     
     ShapeRenderer shrd;
+    ShapeRenderer renderer;
     
     /*
      * Constructeurs
@@ -46,11 +49,13 @@ public class GameScreen implements Screen {
         
         // Designer
         shrd = new ShapeRenderer();
+        renderer = new ShapeRenderer();
     }
 	
 	@Override
 	public void render(float delta)
 	{
+		// Fond
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
@@ -58,7 +63,6 @@ public class GameScreen implements Screen {
 		    stage.draw();
 		    
 			Gdx.gl10.glLineWidth(2);
-			//shrd.setProjectionMatrix(camera.combined);
 			shrd.begin(ShapeType.Line);
 				float positionChat = Gdx.graphics.getWidth()*4/5;
 				shrd.setColor(0, 0, 0, 1);
@@ -101,9 +105,30 @@ public class GameScreen implements Screen {
 	
 	public void showPlateau()
 	{
+		drawHexagon(100,500,40);
+		drawHexagon(180,500,40);
+		drawHexagon(260,500,40);
+	}
+	
+	// Dessine un hexagone en fonction de son centre (x,y) et de son rayon r
+	public void drawHexagon(float x, float y, float r)
+	{
+		float rac = (float) (Math.sqrt(3)/2);
+		
+		Gdx.gl10.glLineWidth(2);
 		shrd.begin(ShapeType.Line);
-		shrd.setColor(0, 0, 0, 1);
-		shrd.line(50, 0, 50, Gdx.graphics.getHeight());
+			shrd.setColor(0, 0, 0, 1);
+			
+			shrd.polygon(
+				new float[] {
+					x, 			 y - r,
+					x + rac * r, y - 0.5f * r,
+					x + rac * r, y + 0.5f * r,
+					x, 			 y + r,
+					x - rac * r, y + 0.5f * r,
+					x - rac * r, y - 0.5f * r
+				}
+			);
 		shrd.end();
 	}
 }
