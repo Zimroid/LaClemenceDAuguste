19c49468ab90a6bef4d9807bc745990ea2753d65
Desmu <desmu.cs@gmail.com>
2014-04-03 15:48:24 +0200

    Liste des parties créées entamée.

--- B-ClientWeb/Client-Web/view/joinGameView.php
+++ B-ClientWeb/Client-Web/view/joinGameView.php
@@ -1,3 +1,6 @@
 <h2>Liste des parties disponibles :</h2>
 
-<p>En Construction !</p>
+<ul id="joinGameView">
+	<p>Cliquez sur le nom d'une partie pour la rejoindre.</p>
+	<input type="button" onclick="gameList()" value="Rafraîchir">
+</ul>
--- B-ClientWeb/Client-Web/view/newGameView.php
+++ B-ClientWeb/Client-Web/view/newGameView.php
@@ -1,5 +1,5 @@
 <h2>Création d'une nouvelle partie</h2>
 
 <label for="gameName">Nom de la partie : </label>
-<input type="text" name="gameName" id="gameName" />
+<input type="text" name="gameName" id="gameName" onkeypress="if (event.keyCode == 13) gameCreate()" />
 <button onclick="gameCreate()" >Créer</button>
