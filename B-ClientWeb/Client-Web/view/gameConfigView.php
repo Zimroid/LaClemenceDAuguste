5adc2577da3fea7f1529306c7889ef0df88bc464
Nelyae <celia.kocher4@gmail.com>
2014-04-04 11:11:40 +0200

    game.js et gameconfigview
    .htaccess pour la forme

--- B-ClientWeb/Client-Web/view/gameConfigView.php
+++ B-ClientWeb/Client-Web/view/gameConfigView.php
@@ -1,6 +1,5 @@
 <h2>Panneau de configuration de la partie</h2>
 <form autocomplete="off" onsubmit="return false;" >
-	<!--<textarea type="text" id="message" name="message" onKeyPress="toucheEntree(event)"></textarea>-->
 	<input type="hidden" id="game_id" />
 	<label for="player_number">Nombre de joueurs :</label>
 	<input type="number" id="player_number" name="player_number" onfocusout="gameConfig();" />
@@ -15,26 +14,30 @@
 				<p id="title1" name="title1">Team 1</p>
 				<button id="button1" name="button1" onClick="newPlayer(1,2)">Nouveau joueur</button>
 				<div name="player" id="player1_1">
-					<select name="player" class="1">
+					<select name="playerName" class="1" onselectionchange="gameConfig();">
 						<option selected="selected">Joueur</option>
 						<option>Admin</option>
 					</select>
 					<label for="legion_number">Nombre de légions</label>
-					<input type="number" min="1" step="1" value="1" name="legion_number" class="1" />
-					<select name="pawn" class="1">
+					<input type="number" min="1" step="1" value="1" name="legion_number" class="1" onfocusout="gameConfig();" />
+					<select name="pawn" class="1" onselectionchange="gameConfig();">
 						<option selected="selected">Forme du pion</option>
 						<option>Carré</option>
 						<option>Cercle</option>
 						<option>Triangle</option>
 					</select>
-					<label for="color">Couleur du pion</label>
-					<input name="color" class="1 color" />
-					<select name="position" class="1">
+					<select name="color" class="1">
+						<option selected="selected">Couleur du pion</option>
+						<option name="1">Rouge</option>
+						<option name="2">Vert</option>
+						<option name="3">Bleu</option>
+					</select>
+					<select name="position" class="1" onselectionchange="gameConfig();">
 						<option selected="selected">Position sur le plateau</option>
-						<option>Bas droit</option>
-						<option>Bas gauche</option>
-						<option>Haut droit</option>
-						<option>Haut gauche</option>
+						<option name="3">Bas droit</option>
+						<option name="4">Bas gauche</option>
+						<option name="1">Haut droit</option>
+						<option name="0">Haut gauche</option>
 					</select>
 				</div>
 			</div>
@@ -43,9 +46,9 @@
 				<button id="button2" name="button2" onClick="newPlayer(2,1)">Nouveau joueur</button>
 			</div>
 		</div>
-		<form id="noTeam" name="noTeam">
+		<div id="noTeam" name="noTeam">
 			<p>Spectateurs</p>
-		</form>
+		</div>
 	</fieldset>
 	<input type="submit" id="valid" value="GO !" />
 </form>
