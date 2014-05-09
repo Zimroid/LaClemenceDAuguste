64502372314f033edc03e01aa77e03d8eaf9401e
Zwyk <erb.fel@mail.com>
2014-05-09 17:42:15 +0200

    IA v1.1.1
    - Gestion du cas où un pion ne pourrait pas bouger

--- A-ServeurAuguste/src/auguste/engine/ia/IA.java
+++ A-ServeurAuguste/src/auguste/engine/ia/IA.java
@@ -58,7 +58,7 @@ public class IA {
                     m = pseudoRandomMove(b,l);
                     break;
             }
-            res.add(new Action(l,m));
+            if(m != null) res.add(new Action(l,m));
         }
         endBotTurn(b);
         return res;
@@ -82,7 +82,8 @@ public class IA {
             armorOk = (pawn instanceof Soldier)?!((Soldier)pawn).isArmored():false;
             cells = game.nearbyEmptyCells(game.friendlyGroup(pawn),armorOk);
             
-            return new Movement(pawn,cells.get(rand.nextInt(cells.size())));
+            if(cells.isEmpty()) return null;
+            else return new Movement(pawn,cells.get(rand.nextInt(cells.size())));
         }
         else return null;
     }
@@ -108,7 +109,8 @@ public class IA {
                 cells = game.nearbyEmptyCells(game.friendlyGroup(pawn),armorOk);
             }
             
-            return new Movement(pawn,cells.get(rand.nextInt(cells.size())));
+            if(cells.isEmpty()) return null;
+            else return new Movement(pawn,cells.get(rand.nextInt(cells.size())));
         }
         else return null;
     }
